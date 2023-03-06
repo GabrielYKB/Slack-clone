5308fe6c1a018d840fc75d4e3511926ab3e14249
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-03-06 14:19:23 +0100

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Channel from "./views/Channel";
+export default function App() {
+    return (
+        <div className="main-container">
+            <Sidebar />
+            <Routes>
+                <Route
+                    path="/"
+                    element={<div className="chat-section">chat section</div>}
+                />
+                <Route path="/:id" element={<Channel />} />
+            </Routes>
+        </div>
+    );
+}
---
+++ server/index.js
@@ -0,0 +1,65 @@
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import cors from "cors";
+
+app.use(cors({ origin: "*" }));
+app.get("/", (req, res) => {
+    res.send("Victor");
+});
+
+const channelsShecma = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 30,
+        },
+    },
+    { timestamps: true }
+);
+
+const Channel = mongoose.model("channels", channelsShecma);
+
+app.get("/channels", async (req, res) => {
+    const channels = await Channel.find();
+    res.send(channels);
+});
+
+const messageSchema = new mongoose.Schema(
+    {
+        text: {
+            type: String,
+            required: true,
+            minLength: 1,
+        },
+        user: {
+            name: {
+                type: String,
+                required: true,
+            },
+            image: {
+                type: String,
+                required: true,
+            },
+        },
+        channelId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "channels",
+        },
+    },
+    { timestamps: true }
+);
+const Message = mongoose.model("messages", messageSchema);
+
+app.get("/channels/:id", async (req, res) => {
+    const messages = await Message.find({ channelId: req.params.id });
+    res.send(messages);
+});
+
+app.listen(3000, async () => {
+    await mongoose.connect(
+        "mongodb+srv://user:admin@cluster0.sttbdxj.mongodb.net/?retryWrites=true&w=majority"
+    );
+});
