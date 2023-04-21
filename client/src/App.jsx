4006e33d4afec881047ece04ace47b08d902f68a
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-04-21 15:13:20 +0200

    visual improvments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,27 +4,34 @@ import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Channel from "./views/Channel";
 export default function App() {
-  const [name, setName] = useState("");
+    const [name, setName] = useState("");
 
-  return (
-    <div className="main-container">
-      {name ? (
-        <>
-          <Sidebar />
-          <Routes>
-            <Route
-              path="/"
-              element={<div className="chat-section">chat section</div>}
-            />
-            <Route path="/:id" element={<Channel username={name} />} />
-          </Routes>
-        </>
-      ) : (
-        <div>
-          <h1>Välkommen till min Slack-clon</h1>
-          <button onClick={() => setName("Gabriel")}>Login as Gabriel</button>
+    return (
+        <div className="main-container">
+            {name ? (
+                <>
+                    <Sidebar />
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <div className="chat-section">chat section</div>
+                            }
+                        />
+                        <Route
+                            path="/:id"
+                            element={<Channel username={name} />}
+                        />
+                    </Routes>
+                </>
+            ) : (
+                <div className="welcome">
+                    <h1>Välkommen till min Slack-clon</h1>
+                    <button onClick={() => setName("Guest")}>
+                        Login as Guest
+                    </button>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,25 +4,25 @@ import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 
 export default function sidebar() {
-  const { data: channels } = useQuery({
-    queryKey: ["channels"],
-    queryFn: () =>
-      fetch("http://localhost:3000/channels").then((data) => data.json()),
-  });
+    const { data: channels } = useQuery({
+        queryKey: ["channels"],
+        queryFn: () =>
+            fetch("http://localhost:3000/channels").then((data) => data.json()),
+    });
 
-  return (
-    <div className="main-container">
-      <div className="sidebar">
-        <h1 className="sidebar-header">Gabriel Yakoub</h1>
-        {channels
-          ? channels.map((channel) => (
-              <Link to={`/${channel._id}`} className="sidebar-row">
-                <Hash />
-                {channel.name}
-              </Link>
-            ))
-          : "Loading..."}
-      </div>
-    </div>
-  );
+    return (
+        <div className="main-container">
+            <div className="sidebar">
+                <h1 className="sidebar-header">Logged in as Guest</h1>
+                {channels
+                    ? channels.map((channel) => (
+                          <Link to={`/${channel._id}`} className="sidebar-row">
+                              <Hash />
+                              {channel.name}
+                          </Link>
+                      ))
+                    : "Loading..."}
+            </div>
+        </div>
+    );
 }
